9d0a0643dff8ed22e01c559f838063debca5cd75
Haner Camilo <49127465+copaamerica@users.noreply.github.com>
2025-10-16 21:36:46 -0500

    Commit desde la rama temporal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("¡Hola mundo dev senior commit 2!");
+        System.out.println("Modificación para nueva rama");
     }
 }
