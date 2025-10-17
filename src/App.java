80cc42f45549764166a8bd23e1bbdabe401de45e
Haner Camilo <49127465+copaamerica@users.noreply.github.com>
2025-10-16 21:09:37 -0500

    Commit 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("¡Hola mundo dev senior!");
+        System.out.println("¡Hola mundo dev senior commit 2!");
     }
 }
